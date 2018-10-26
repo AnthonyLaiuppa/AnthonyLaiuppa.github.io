29a5275c90376496459ae0a7ad8e3fa139eb73a1
AnthonyLaiuppa <anthonylaiuppa@gmail.com>
2018-10-26 14:50:16 -0700

    MapSnatch Post

--- sw.js
+++ sw.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["/app-7e7be0bb5f1e62b41140.js","f0dbf1c59a96c008cbd477def0d5ca80"],["/commons-3f2fa4997e5f1e65a3b7.js","37915e36ccbd429ec4a6b98e2ea834e1"],["/commons-fb4afb5c92dcdc3034fe.js","6e397eddac156760c5f54fdf96917183"],["/index.html","f3c7b40beed89cc1e4d8adff4bdfbb64"],["/manifest.webmanifest","f38bcd0017d6fe8c34dec3c9294624b5"],["/offline-plugin-app-shell-fallback/index.html","c7cc514a840484fb49ccaedf0e8ec23e"]];
+var precacheConfig = [["/app-63a1e83da348ac316ddd.js","427224f093b9aa05194a391d5bcf7d07"],["/app-7e7be0bb5f1e62b41140.js","f0dbf1c59a96c008cbd477def0d5ca80"],["/commons-3f2fa4997e5f1e65a3b7.js","37915e36ccbd429ec4a6b98e2ea834e1"],["/commons-fb4afb5c92dcdc3034fe.js","6e397eddac156760c5f54fdf96917183"],["/index.html","e7db48c72903e6b874f5f8ce4d5efd2d"],["/manifest.webmanifest","f38bcd0017d6fe8c34dec3c9294624b5"],["/offline-plugin-app-shell-fallback/index.html","33e88db98034859b1db6294bd06bdd69"]];
 var cacheName = 'sw-precache-v3-gatsby-plugin-offline-' + (self.registration ? self.registration.scope : '');
 
 
